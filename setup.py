26c9a844cb4e0a2813451b366b7122220d9d70fd
João Santos <jmcs@jsantos.eu>
2014-12-14 00:23:58 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+setup(
+    name = "rod",
+    packages = ["rod"],
+    version = "2014.12.14",
+    description = "Redis Object Database",
+    author = "João Santos",
+    author_email = "jmcs@jsantos.eu",
+    url = "http://jsantos.eu",
+    install_requires=["pyyaml", "redis"],
+    keywords = ["redis"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+Redis Object Database
+---------------------
+
+Use Redis as a python object database.
+"""
+)
